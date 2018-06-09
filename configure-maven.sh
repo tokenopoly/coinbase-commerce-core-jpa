619cdd7a371ecdedccdfdf811a3a6f96a3e4fbae
Ezra Epstein <ezra@tokenopoly.com>
2018-06-08 17:38:45 -0700

    remove files related to old source repo

--- configure-maven.sh
+++
@@ -1,6 +0,0 @@
-sed -i~ "/<servers>/ a\
-<server>\
-  <id>datavolo</id>\
-  <username>${MAVEN_USERNAME}</username>\
-  <password>${MAVEN_PASSWORD}</password>\
-</server>" /usr/share/maven/conf/settings.xml
